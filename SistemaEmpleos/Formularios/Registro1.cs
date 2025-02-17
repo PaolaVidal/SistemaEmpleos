f8eb0e6b85ad11acda589bfd959a94225a3d1f66
Paola Vidal <paola.vidal@catolica.edu.sv>
2025-02-17 14:52:23 -0600

    login y registro pasante

--- SistemaEmpleos/Formularios/Registro1.cs
+++ SistemaEmpleos/Formularios/Registro1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace SistemaEmpleos
 {
-	public partial class Form1 : Form
+	public partial class Registro1 : Form
 	{
-		public Form1()
+		public Registro1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
---
+++ SistemaEmpleos/Formularios/RegistroNormal.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace SistemaEmpleos.Formularios
+{
+	partial class RegistroNormal
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel1.Location = new System.Drawing.Point(1, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1284, 696);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.Control;
+			this.panel2.Controls.Add(this.button2);
+			this.panel2.Controls.Add(this.label5);
+			this.panel2.Controls.Add(this.button1);
+			this.panel2.Controls.Add(this.textBox5);
+			this.panel2.Controls.Add(this.textBox3);
+			this.panel2.Controls.Add(this.textBox2);
+			this.panel2.Controls.Add(this.textBox1);
+			this.panel2.Controls.Add(this.label7);
+			this.panel2.Controls.Add(this.label6);
+			this.panel2.Controls.Add(this.label4);
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.panel2.Location = new System.Drawing.Point(356, 50);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(572, 596);
+			this.panel2.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label1.Location = new System.Drawing.Point(235, 33);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(103, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Regístrate";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label2.Location = new System.Drawing.Point(54, 112);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 24);
+			this.label2.TabIndex = 3;
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label3.Location = new System.Drawing.Point(85, 160);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(74, 24);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Correo";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label4.Location = new System.Drawing.Point(85, 227);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(116, 24);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Contraseña";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label6.Location = new System.Drawing.Point(85, 101);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(85, 24);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "Nombre";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label7.Location = new System.Drawing.Point(85, 290);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(99, 24);
+			this.label7.TabIndex = 8;
+			this.label7.Text = "Dirección";
+			this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(251, 101);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(239, 26);
+			this.textBox1.TabIndex = 9;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(251, 160);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(239, 26);
+			this.textBox2.TabIndex = 10;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(251, 227);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(239, 26);
+			this.textBox3.TabIndex = 11;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox5.Location = new System.Drawing.Point(251, 290);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(239, 26);
+			this.textBox5.TabIndex = 13;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(209, 356);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(159, 39);
+			this.button1.TabIndex = 14;
+			this.button1.Text = "Regístrate";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(220, 460);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(148, 20);
+			this.label5.TabIndex = 15;
+			this.label5.Text = "Eres una empresa?";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(158, 492);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(260, 39);
+			this.button2.TabIndex = 16;
+			this.button2.Text = "Regístrate como Empresa";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// RegistroNormal
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1284, 694);
+			this.Controls.Add(this.panel1);
+			this.Name = "RegistroNormal";
+			this.Text = "RegistroNormal";
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button2;
+	}
+}
