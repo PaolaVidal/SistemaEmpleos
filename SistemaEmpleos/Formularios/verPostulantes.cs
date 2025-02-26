748f594c356ed90e7b21d86ccdd939c54e2b0b1c
Rodolfo Rivas Rodriguez <128103152+FOFOCode@users.noreply.github.com>
2025-02-26 16:03:00 -0600

    Volvemos hacer el ver postulantes

--- SistemaEmpleos/Formularios/verPostulantes.cs
+++ SistemaEmpleos/Formularios/verPostulantes.cs
@@ -18,25 +18,6 @@ namespace SistemaEmpleos.Formularios
         public verPostulantes()
         {
             InitializeComponent();
-
-            // Instanciamos la conexión con los parámetros del servidor y base de datos
-            crud = new VerPostulanteCRUD(@"(Localdb)\rodolfo server", "Empleo");
-
-            // Ahora sí podemos llamar a VerificarConexion
-            crud.VerificarConexion();
-            crud.GetAllPostulantes(this.dataGridView1,2);
-
-            this.cbFiltro.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbFiltro.Items.Add("nombre");
-            cbFiltro.Items.Add("telefono");
-            cbFiltro.Items.Add("habilidades");
-            cbFiltro.Items.Add("idiomas");
-            cbFiltro.Items.Add("titulo");
-            cbFiltro.Items.Add("institucion");
-            cbFiltro.Items.Add("fecha");
-            cbFiltro.Items.Add("puesto");
-            cbFiltro.Items.Add("empresa");
-            cbFiltro.SelectedIndex = 0; // Seleccionar el primer filtro por defecto
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -56,8 +37,6 @@ namespace SistemaEmpleos.Formularios
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string filtro = cbFiltro.SelectedItem?.ToString();  // Obtenemos el filtro seleccionado como string
-            string textoBusqueda = txtDatoFiltrar.Text;  // Obtenemos el texto ingresado en el TextBox
 
         }
 
@@ -73,15 +52,12 @@ namespace SistemaEmpleos.Formularios
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            crud.GetAllPostulantes(this.dataGridView1, 2);
+            
         }
 
         private void txtDatoFiltrar_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ' ')
-            {
-                e.Handled = true; // Bloquea la tecla si no cumple la condición
-            }
+           
         }
     }
 }
